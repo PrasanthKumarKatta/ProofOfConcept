26a6ba88e5057b133294768dcf8f6c36375a6bab
Katta Prasanth Kumar <39497825+PrasanthKumarKatta@users.noreply.github.com>
2020-01-16 13:55:11 +0530

    added MVVM pattern
    API data loading with ViewModel for Data persistance on LifeCycle

--- app/src/main/java/com/kpcode4u/prasanthkumarkatta/proofofconcept/view/MainActivity.java
+++ app/src/main/java/com/kpcode4u/prasanthkumarkatta/proofofconcept/view/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 }
                 countryList = countryResponse.getCountryList();
                 System.out.println("kpk: length: " + countryList.size());
-                //Todo:kpk: parsing List to Recyclerview Adapter
+                //Todo:kpk: parsing List to RecyclerviewAdapter
                 if (countryList.size() != 0) {
                     adapter = new CountryAdapter(MainActivity.this, countryList);
                     recyclerView.setAdapter(adapter);
